5977bc3e153155b22b0ce2602f096809ec8e9562
Dicki Rizki Amarullah <dickirizki21@gmail.com>
2019-11-30 15:51:10 +0700

    fixing bug conflict

--- application/views/layout/nav.php
+++ application/views/layout/nav.php
@@ -1,39 +1,28 @@
-					    < swap file by the name "application/views/layout/.nav.php.swp"
-          owned by: user   dated: Sat Nov 30 1323:29 2019
-         file name: /c/xampp/htdocs/padmaindonesia/application/views/layout/nav.php
-          modified: YES
-         user name: user   host name: OMDIK
-        process ID: 1544
-While opening file "application/views/layout/nav.php"
-             dated: Sat Nov 30 13:28:13 2019
-      NEWER than swap file!
+</div><!-- End .header-dropown -->
+                    </div><!-- End .header-right -->
+                </div><!-- End .container -->
+            </div><!-- End .header-top -->
 
-(1) Another program may be editing the same file.  If this is the case,
-    be careful not to end up with two different instances of the same
-    file when making changes.  Quit, or continue with caution.
-(2) An edit session for this file crashed.
-    If this is the case, use ":recover" or "vim -r application/views/layout/nav.php"
-    to recover the changes (see ":help recovery").
-    If you did this already, delete the swap file "application/views/layout/.nav.php.swp"
-    to avoid this message.
+            <div class="header-middle">
+                <div class="container">
+                    <div class="header-left">
+                        <a href="index.html" class="logo">
+                            <img src="<?php echo base_url() ?>assets/padma-home/demo-4/assets/images/logo.png" alt="Padmaindonesia logo">
+                        </a>
+                    </div><!-- End .header-left -->
 
-Swap file "application/views/layout/.nav.php.swp" already exists!
-[O]pen Read-Only, (E)dit anyway, (R)ecover, (D)elete it, (Q)uit, (A)bort:                                                                                                                                               Found a swap file by the name "application/views/layout/.nav.php.swp"
-          owned by: user   dated: Sat Nov 30 13:23:29 2019
-         file name: /c/xampp/htdocs/padmaindonesia/application/views/layout/nav.php
-          modified: YES
-         user name: user   host name: OMDIK
-        process ID: 1544
-While opening file "application/views/layout/nav.php"
-             dated: Sat Nov 30 13:28:13 2019
-      NEWER than swap file!
-
-(1) Another program may be editing the same file.  If this is the case,
-    be careful not to end up with two different instances of the same
-    file when making changes.  Quit, or continue with caution.
-(2) An edit session for this file crashed.
-    If this is the case, use ":recover" or "vim -r application/views/layout/nav.php"
-option value="13">- Men</option>
+                    <div class="header-center">
+                        <div class="header-search">
+                            <a href="index.html#" class="search-toggle" role="button"><i class="icon-magnifier"></i></a>
+                            <form action="#" method="get">
+                                <div class="header-search-wrapper">
+                                    <input type="search" class="form-control" name="q" id="q" placeholder="Search..." required>
+                                    <div class="select-custom">
+                                        <select id="cat" name="cat">
+                                            <option value="">All Categories</option>
+                                            <option value="4">Fashion</option>
+                                            <option value="12">- Women</option>
+                                            <option value="13">- Men</option>
                                             <option value="66">- Jewellery</option>
                                             <option value="67">- Kids Fashion</option>
                                             <option value="5">Electronics</option>
@@ -63,202 +52,24 @@ option value="13">- Men</option>
                 <div class="container">
                     <nav class="main-nav">
                         <ul class="menu sf-arrows">
-<<<<<<< HEAD
-                            <li>
-                                <a href="<?php echo base_url('home') ?>">Beranda</a>
-                            </li>
-
-                            <li>
-                                <a href="<?php echo base_url('tentang') ?>">Tentang Kami</a>
-                            </li>
-                            <li class="megamenu-container">
-                                <a href="product.html" class="sf-with-ul">Produk</a>
-                                <div class="megamenu">
-                                    <div class="row">
-                                        <div class="col-lg-8">
-                                            <div class="row">
-                                                <div class="col-lg-4">
-                                                    <div class="menu-title">
-                                                        <a href="index.html#">Variations</a>
-                                                    </div>
-                                                    <ul>
-                                                        <li><a href="product.html">Horizontal Thumbnails</a></li>
-                                                        <li><a href="product-full-width.html">Vertical Thumbnails<span class="tip tip-hot">Hot!</span></a></li>
-                                                        <li><a href="product.html">Inner Zoom</a></li>
-                                                        <li><a href="product-addcart-sticky.html">Addtocart Sticky</a></li>
-                                                        <li><a href="product-sidebar-left.html">Accordion Tabs</a></li>
-                                                    </ul>
-                                                </div><!-- End .col-lg-4 -->
-                                                <div class="col-lg-4">
-                                                    <div class="menu-title">
-                                                        <a href="index.html#">Variations</a>
-                                                    </div>
-                                                    <ul>
-                                                        <li><a href="product-sticky-tab.html">Sticky Tabs</a></li>
-                                                        <li><a href="product-simple.html">Simple Product</a></li>
-                                                        <li><a href="product-sidebar-left.html">With Left Sidebar</a></li>
-                                                    </ul>
-                                                </div><!-- End .col-lg-4 -->
-                                                <div class="col-lg-4">
-                                                    <div class="menu-title">
-                                                        <a href="index.html#">Product Layout Types</a>
-                                                    </div>
-                                                    <ul>
-                                                        <li><a href="product.html">Default Layout</a></li>
-                                                        <li><a href="product-extended-layout.html">Extended Layout</a></li>
-                                                        <li><a href="product-full-width.html">Full Width Layout</a></li>
-                                                        <li><a href="product-grid-layout.html">Grid Images Layout</a></li>
-                                                        <li><a href="product-sticky-both.html">Sticky Both Side Info<span class="tip tip-hot">Hot!</span></a></li>
-                                                        <li><a href="product-sticky-info.html">Sticky Right Side Info</a></li>
-                                                    </ul>
-                                                </div><!-- End .col-lg-4 -->
-                                            </div><!-- End .row -->
-                                        </div><!-- End .col-lg-8 -->
-                                        <div class="col-lg-4">
-                                            <div class="banner">
-                                                <a href="index.html#">
-                                                    <img src="assets/images/menu-banner.jpg" alt="Menu banner" class="product-promo">
-                                                </a>
-                                            </div><!-- End .banner -->
-                                        </div><!-- End .col-lg-4 -->
-                                    </div><!-- End .row -->
-                                </div><!-- End .megamenu -->
-                            </li>
-                            <li>
-                                <a href="index.html#" class="sf-with-ul">Galeri</a>
-
-                                <ul>
-                                    <li><a href="cart.html">Shopping Cart</a></li>
-                                    <li><a href="index.html#">Checkout</a>
-                                        <ul>
-                                            <li><a href="checkout-shipping.html">Checkout Shipping</a></li>
-                                            <li><a href="checkout-shipping-2.html">Checkout Shipping 2</a></li>
-                                            <li><a href="checkout-review.html">Checkout Review</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="index.html#">Dashboard</a>
-                                        <ul>
-                                            <li><a href="dashboard.html">Dashboard</a></li>
-                                            <li><a href="my-account.html">My Account</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="about.html">About Us</a></li>
-                                    <li><a href="index.html#">Blog</a>
-                                        <ul>
-                                            <li><a href="blog.html">Blog</a></li>
-                                            <li><a href="single.html">Blog Post</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="contact.html">Contact Us</a></li>
-                                    <li><a href="index.html#" class="login-link">Login</a></li>
-                                    <li><a href="forgot-password.html">Forgot Password</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="index.html#" class="sf-with-ul">Berita</a>
-                                <ul>
-                                    <li><a href="index.html#">Header Types</a></li>
-                                    <li><a href="index.html#">Footer Types</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="index.html#" class="sf-with-ul">Toko Kami</a>
-=======
                             <li class="active"><a href="<?php echo base_url('home') ?>">Beranda</a></li>
                             <li class="active"><a href="<?php echo base_url('tentang') ?>">Tentang Kami</a></li>
                             <li><a href="<?php echo base_url('produk') ?>">Produk</a>
->>>>>>> d742630865dc9f256cc73e0a827c037f8454f785
                                 <ul>
                                     <li><a href="<?php echo base_url('produk1') ?>">Produk 1</a></li>
                                     <li><a href="<?php echo base_url('produk1') ?>">Produk 2</a></li>
                                     <li><a href="<?php echo base_url('terkini') ?>">Produk 3</a></li>
                                     <li><a href="<?php echo base_url('terkini') ?>">Produk 4</a></li>
-                                    <li><a href="<?php 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/header><!-- End .header -->
+                                    <li><a href="<?php echo base_url('terkini') ?>">Produk 5</a></li>
+                                    <li><a href="<?php echo base_url('terkini') ?>">Produk 6</a></li>
+                                </ul>
+                            </li>
+                            <li class="active"><a href="<?php echo base_url('galeri') ?>">Galeri</a></li>
+                            <li><a href="<?php echo base_url('berita') ?>">Berita</a></li>
+                            <li><a href="<?php echo base_url('toko') ?>">Toko Kami</a></li>
+                            <li class="active"><a href="<?php echo base_url('kontak') ?>">Kontak Kami</a></li>
+                        </ul>
+                    </nav>
+                </div><!-- End .header-bottom -->
+            </div><!-- End .header-bottom -->
+        </header><!-- End .header -->
