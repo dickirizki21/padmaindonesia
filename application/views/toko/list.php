fa8c46cd9563615c7e1f0a5879c63a9764989061
diahputriramadhani <diahputriramadhani98@gmail.com>
2019-12-11 09:34:16 +0700

    Editing toko/list and deleting galeri/coba

--- application/views/toko/list.php
+++ application/views/toko/list.php
@@ -1,27 +1,59 @@
-<main class="main">
-        <div class="container" style="padding-bottom: 60px">
-            <center><h2 class="carousel-title" style="padding-top: 50px">PADMA INDONESIA TERSEDIA DI BEBERAPA KOTA</h2></center>
+<div class="container" style="padding-bottom: 60px">
+    <center><h2 class="carousel-title" style="padding-top: 50px"><?php echo $title ?></h2></center>
+</div>
+    <div class="home-top-section">
+        <div class="container" style="padding-left: 120px">
+            <div class="row">
+                
+                <div class="col-md-4">
+                    <div class="cat-box">
+                        <h4 style="color: #fc0341">KOTA TOKO DIDIRIKAN</h4>
+                        <h2>NAMA TOKO</h2>
+                        <p>Alamat lengkap sekali toko</p>
+                    </div><!-- End .cat-box -->
+                </div><!-- End .col-md-4 -->
 
-        <div class="product-wrapper">
-            <div class="col-8 col-md-4">
-	            <center>
-                    <h4 style="color: #fc0341">KOTA TOKO DIDIRIKAN</h4>
-    	            <h2>TOKO</h2>
-    	            <p>Alamat lengkap sekali toko</p>
-        	    </center>
-                <center>
-    	            <h4 style="color: #fc0341; padding-top: 30px">KOTA TOKO DIDIRIKAN</h4>
-    	            <h2>TOKO</h2>
-    	            <p>Alamat lengkap sekali toko</p>
-        	    </center>
-                <center>
-	            <h4 style="color: #fc0341; padding-top: 30px">KOTA TOKO DIDIRIKAN</h4>
-	            <h2>TOKO</h2>
-	            <p>Alamat lengkap sekali toko</p>
-                </center>
-        	</div>
+                <div class="col-md-4">
+                    <div class="cat-box">
+                        <h4 style="color: #fc0341">KOTA TOKO DIDIRIKAN</h4>
+                        <h2>NAMA TOKO</h2>
+                        <p>Alamat lengkap sekali toko</p>
+                    </div><!-- End .cat-box -->
+                </div><!-- End .col-md-4 -->
 
-        <div class="row row-sm category-grid">
+                <div class="col-md-4">
+                    <div class="cat-box">
+                        <h4 style="color: #fc0341">KOTA TOKO DIDIRIKAN</h4>
+                        <h2>NAMA TOKO</h2>
+                        <p>Alamat lengkap sekali toko</p>
+                    </div><!-- End .cat-box -->
+                </div><!-- End .col-md-4 -->
+
+                <div class="col-md-4">
+                    <div class="cat-box">
+                        <h4 style="color: #fc0341; padding-top: 50px">KOTA TOKO DIDIRIKAN</h4>
+                        <h2>NAMA TOKO</h2>
+                        <p>Alamat lengkap sekali toko</p>
+                    </div><!-- End .cat-box -->
+                </div><!-- End .col-md-4 -->
+
+                <div class="col-md-4">
+                    <div class="cat-box">
+                        <h4 style="color: #fc0341; padding-top: 50px">KOTA TOKO DIDIRIKAN</h4>
+                        <h2>NAMA TOKO</h2>
+                        <p>Alamat lengkap sekali toko</p>
+                    </div><!-- End .cat-box -->
+                </div><!-- End .col-md-4 -->
+
+                <div class="col-md-4">
+                    <div class="cat-box">
+                        <h4 style="color: #fc0341; padding-top: 50px">KOTA TOKO DIDIRIKAN</h4>
+                        <h2>NAMA TOKO</h2>
+                        <p>Alamat lengkap sekali toko</p>
+                    </div><!-- End .cat-box -->
+                </div><!-- End .col-md-4 -->
+
+            </div><!-- End .row -->
         </div><!-- End .container -->
     </div>
-</main>
+<div class="mb-3 mb-lg-5"></div><!-- margin -->
